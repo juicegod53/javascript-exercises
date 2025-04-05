facfa61425ae04211a298f25665cbf20dd0b9149
Abhi Kalakoti <abhikalakoti@gmail.com>
2025-04-05 12:21:48 +0100

    Completed 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reversed_string = ''
+    for (let i = string.length-1; i >= 0; i--) {
+        reversed_string += string[i]
+    }
+    return reversed_string
 };
 
 // Do not edit below this line
