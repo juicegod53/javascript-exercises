2ce16840fa7806d4d20a424e2e4cbb078f3e72b9
Abhi Kalakoti <abhikalakoti@gmail.com>
2025-04-05 18:53:58 +0100

    Completed 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(int1, int2) {
+    if (typeof(int1) != "number" || typeof(int2) != "number" || int1 < 0 || int2 < 0 || int1 % 1 != 0 || int2 % 1 != 0) {
+        return "ERROR"
+    }
+    vals = [int1, int2]
+    vals.sort()
+    sum = (vals[1] * (vals[1] +1) / 2) - ((vals[0] - 1) * vals[0] / 2)
+    return sum
 };
 
 // Do not edit below this line
