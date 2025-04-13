3afce2f65703519e195977d22a71615fd08fdf2f
Abhi Kalakoti <abhikalakoti@gmail.com>
2025-04-13 13:20:07 +0100

    Completed 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    biggestAge = -1
+    for (let i = 0; i < people.length; i++) {
+        if (people[i].yearOfDeath) {
+            age = people[i].yearOfDeath - people[i].yearOfBirth
+        } else {
+            age = 2025 - people[i].yearOfBirth
+        }
+        if (age > biggestAge) {
+            person = people[i]
+            biggestAge = age
+        }
+    }
+    return person
 };
 
 // Do not edit below this line
