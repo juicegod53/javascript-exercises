a1ad7c226ada021c95274b629b4400cece5eaf6a
Abhi Kalakoti <abhikalakoti@gmail.com>
2025-04-13 12:51:52 +0100

    Completed 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n < 0) {
+        return "OOPS"
+    } else if (n == 0) {
+        return 0
+    }
+    n = parseInt(n)
+    let fib = [1,1]
+    let length = fib.length
+    while (length < n) {
+        fib.push(fib[length-1]+fib[length-2])
+        length = fib.length
+    }
+    return fib[n-1]
 
 };
 
