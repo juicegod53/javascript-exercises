8f72af3a33a6a736c2fac10d726a8d0806b32e9c
Abhi Kalakoti <abhikalakoti@gmail.com>
2025-04-13 12:42:12 +0100

    Completed 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,5 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    return string.toLowerCase().split("").filter(item => !([" ", ".", ",", "!"].includes(item))).join("") == string.toLowerCase().split("").filter(item => !([" ", ".", ",", "!"].includes(item))).reverse().join("")
 };
 
 // Do not edit below this line
