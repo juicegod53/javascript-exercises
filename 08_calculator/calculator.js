64373330fd80055559610a4033ce6861d8212cb9
Abhi Kalakoti <abhikalakoti@gmail.com>
2025-04-13 12:18:33 +0100

    Completed 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a+b
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+  return a-b
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	return nums.reduce((total, num) => total += num, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  return nums.reduce((total, num) => total *= num, 1)
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let nums = []
+  while (a > 1) {
+    nums.push(a)
+    a -= 1
+  }
+  return nums.reduce((total, num) => total *= num, 1)
 };
 
 // Do not edit below this line
